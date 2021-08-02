6e929b4e892b5430d17d0c9203c75057771278a9
qq875998773 <875998773@qq.com>
2021-08-02 23:30:24 +0800

    上传项目

---
+++ inc/VulkanBase/macros.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "vulkan/vulkan.h"
+
+#define VK_CHECK_RESULT(f)																				\
+{																										\
+	VkResult res = (f);																					\
+	if (res != VK_SUCCESS)																				\
+	{																									\
+		std::cout << "Fatal : VkResult is \"" << res << "\" in " << __FILE__ << " at line " << __LINE__ << std::endl; \
+		assert(res == VK_SUCCESS);																		\
+	}																									\
+}
+
+#define GET_INSTANCE_PROC_ADDR(inst, entrypoint)                        \
+{                                                                       \
+	fp##entrypoint = reinterpret_cast<PFN_vk##entrypoint>(vkGetInstanceProcAddr(inst, "vk"#entrypoint)); \
+	if (fp##entrypoint == NULL)                                         \
+	{																    \
+		exit(1);                                                        \
+	}                                                                   \
+}
+
+#define GET_DEVICE_PROC_ADDR(dev, entrypoint)                           \
+{                                                                       \
+	fp##entrypoint = reinterpret_cast<PFN_vk##entrypoint>(vkGetDeviceProcAddr(dev, "vk"#entrypoint));   \
+	if (fp##entrypoint == NULL)                                         \
+	{																    \
+		exit(1);                                                        \
+	}                                                                   \
+}
